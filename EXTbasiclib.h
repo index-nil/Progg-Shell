6a83af1dd6520dec895315f69e1895b984199a24
index-nil <249247145+index-nil@users.noreply.github.com>
2026-01-31 13:12:27 +0400

    Delete EXTbasiclib.h

--- EXTbasiclib.h
+++
@@ -1,8 +0,0 @@
-#ifndef EXT_BASIC_LIB_H
-#define EXT_BASIC_LIB_H
-
-//------------Extension Basic Lib------------//
-int create_pb(char* Text ,int ShowPercent, int value,int chars, char* FullChar, char* VoidChar, char* Borders, int overwrite,int Color);
-
-
-#endif
