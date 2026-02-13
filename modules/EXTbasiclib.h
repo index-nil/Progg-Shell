bf69c4014a451fea05c400c53bdf40ba8a4e2002
index-nil <249247145+index-nil@users.noreply.github.com>
2026-02-13 12:27:46 +0400

    Delete modules/EXTbasiclib.h

--- modules/EXTbasiclib.h
+++
@@ -1,8 +0,0 @@
-#ifndef EXT_BASIC_LIB_H
-#define EXT_BASIC_LIB_H
-
-//------------Extension Basic Lib GPL V3------------//
-int create_pb(char* Text ,int ShowPercent, int value,int chars, char* FullChar, char* VoidChar, char* Borders, int overwrite,int Color);
-
-
-#endif
