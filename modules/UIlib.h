3df426049c50412d19512b2674b285396d01039f
index-nil <249247145+index-nil@users.noreply.github.com>
2026-02-28 21:47:12 +0400

    Delete modules directory

--- modules/UIlib.h
+++
@@ -1,8 +0,0 @@
-#ifndef UI_BASIC_LIB_H
-#define UI_BASIC_LIB_H
-
-//------------Extension Basic Lib GPL V3------------//
-int create_pb(char* Text ,int ShowPercent, int value,int chars, char* FullChar, char* VoidChar, char* Borders, int overwrite, int r,int g, int b);
-int CreateGraph(int Height, int value,int Colums, int overwrite, char *bordes,char *BordesStart, char *BordesEnd ,char *full,char *voidChar, int Voidcolor,int TextColor,int FullColor,int BordesColor);
-
-#endif
